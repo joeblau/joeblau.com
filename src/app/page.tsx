63c86268ba2da7d4a1ac81d88261123bec2eb5aa
Joe Blau <im@joeblau.com>
2023-07-20 22:44:15 -0700

    feat: add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,37 @@ import { curriculumVitae } from "@/lib/models/curriculum-vitae";
 export default function Home() {
   return (
     <main>
-      <div className={"mx-auto max-w-xl px-4 sm:px-6 lg:px-8 text-neutral"}>
-        <div>
-          <h1>Joe Blau</h1>
-          <h2>Founder • Investor</h2>
-        </div>
-
-        <div>
-          {curriculumVitae.map((section) => (
-            <div key={section.id}>
-              <div>{section.title}</div>
-              {section.items.map((item) => (
-                <div key={item.id}>
-                  <div>{item.title}</div>
-                  <div>{item.description}</div>
-                </div>
-              ))}
+      <div className={"mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 text-neutral"}>
+        <code className="lowercase">
+          <div>
+            <h1>Joe Blau</h1>
+            <h2>Founder • Investor</h2>
+            <div>&nbsp;</div>
+          </div>
+          <div>
+            {curriculumVitae.map((section, index) => (
+              <div key={index}>
+                <div>~$ ./blau {section.title}</div>
+                <div>&nbsp;</div>
+                <ul className="pl-8">
+                  {section.items.map((item, index) => (
+                    <li key={index}>
+                      <div className="font-medium inline">
+                        {item.link ? <a href={item.link}>{item.title}</a> : <span>{item.title}</span>}
+                      </div>
+                      <span className="font-light inline">{` - ${item.description}`}</span>
+                    </li>
+                  ))}
+                </ul>
+                <div>&nbsp;</div>
+              </div>
+            ))}
+            <div>
+              ~$
+              <span className="inline blink"> █</span>
             </div>
-          ))}
-        </div>
+          </div>
+        </code>
       </div>
     </main>
   );
