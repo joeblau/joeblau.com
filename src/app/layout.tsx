bdd0cb6d393a026d7c29ba40612cd7629270c1f8
Joe Blau <im@joeblau.com>
2023-07-21 16:45:09 -0700

    feat: fix open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,28 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Joe Blau",
   description: "Founder • Investor",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://joeblau.com",
+    title: "Joe Blau",
+    description: "Founder • Investor",
+    images: [
+      {
+        url: "https://joeblau.com/og.png",
+        width: 1200,
+        height: 600,
+        alt: "Joe Blau",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider attribute="class" defaultTheme="system">
+      <body className="bg-white dark:bg-black text-black dark:text-white">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
